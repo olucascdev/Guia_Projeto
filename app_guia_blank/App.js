7decce7ec4d9003a518ddca326f8351b9fd159c2
Lucas Correia <lucasdevproject@gmail.com>
2024-11-02 12:18:53 -0300

    add arquivos

--- app_guia_blank/App.js
+++ app_guia_blank/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TabNavigation from './App/Navigations/TabNavigation';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <NavigationContainer>
+          <TabNavigation/>
+        </NavigationContainer>
     </View>
   );
 }
@@ -14,7 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
 });

1f65ee104296d0d4a7e10c6cbb8ccd59df3987e7
Lucas Correia <lucasdevproject@gmail.com>
2024-11-02 15:04:16 -0300

    Map

---
+++ app_guia_blank/App/Components/Home/GoogleMapView.js
@@ -0,0 +1,47 @@
+import { View, Text, Dimensions } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { UserLocationContext } from "../../Context/UserLocationContext";
+import PlaceMarker from "./PlaceMarker";
+import Colors from "../../Shared/Colors";
+
+export default function GoogleMapView() {
+    const { location, setLocation } = useContext(UserLocationContext);
+    const [mapRegion, setmapRegion] = useState([]);
+        
+    useEffect(()=>{
+        if(location)
+        {
+            setmapRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0322,
+                longitudeDelta: 0.0421,
+            })
+        }
+      },[location])
+
+  return (
+    <View style={{ marginTop: 20 }}>
+
+        <Text style={{ fontSize: 16, 
+        marginBottom: 10, fontWeight: "650",fontFamily:'raleway-bold' }}>
+        Melhores lugares perto de você!
+        </Text>
+        <View style={{ borderRadius: 20, overflow: "visible" }}>
+            <MapView
+                style={{
+                    width: Dimensions.get("screen").width * 1.2,
+                    height: Dimensions.get("screen").height * 0.23,
+                }}
+                provider={PROVIDER_GOOGLE}
+                showsUserLocation={true}
+                ></MapView>
+        </View>            
+
+      
+    </View>
+
+
+  )
+}
--- app_guia_blank/App/Screens/Home.js
+++ app_guia_blank/App/Screens/Home.js
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../Components/Home/Header';
+import GoogleMapView from '../Components/Home/GoogleMapView';
+
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{paddingTop:40,backgroundColor:'#fff',flex:1
+    }}>
+      <Header/>
+      <GoogleMapView/>
     </View>
   )
 }
